3b97438d06758c927cb9f8fd60fa4403774f61f2
Tologon Eshimkanov <eshimkanovt@wit.edu>
2014-11-07 20:44:52 -0500

    Added Controls class (incomplete).

---
+++ src/tantibus/controls/Controls.java
@@ -0,0 +1,20 @@
+package tantibus.controls;
+
+import org.newdawn.slick.Input;
+
+import tantibus.character.Player;
+import tantibus.level.Level;
+
+public class Controls extends PlayerController {
+	public Controls(Player player) {
+		super(player);
+	}
+	
+	public void controlInput(Input input, int num, Level level) {
+		//TODO follow tutorial
+	}
+	
+	private void controlKeyboardInput(Input input, int num, Level level) {
+		//TODO follow tutorial
+	}
+}
