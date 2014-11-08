554318b21f92f50decef479a03357b7fb3ba7495
Tologon Eshimkanov <eshimkanovt@wit.edu>
2014-11-07 20:16:44 -0500

    Added Level class (incomplete).

---
+++ src/tantibus/level/Level.java
@@ -0,0 +1,59 @@
+package tantibus.level;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.tiled.TiledMap;
+
+import tantibus.character.Player;
+import tantibus.enums.Gravity;
+
+public class Level {
+	private TiledMap map;
+	private ArrayList<Character> characters;
+	private Player protagonist;
+	private ArrayList<LevelObject> levelObjects;
+	private Tile tiles;
+	private Gravity gravity;
+	
+	public Level(String title, Player protagonist) {
+		//TODO with "title"
+		this.protagonist = protagonist;
+	}
+	public void loadObjects() {
+		//TODO follow tutorial
+	}
+	public void addCharacters(Character[] characters) {
+		//TODO follow tutorial
+	}
+	public ArrayList<Character> getCharacters() {
+		return characters;
+	}
+	public void addLevelObject(LevelObject[] levelObjects) {
+		//TODO follow tutorial
+	}
+	public Tile getTiles() {
+		return tiles;
+	}
+	/*TODO follow tutorial
+	public int getXOffset() {
+		//TODO follow tutorial
+	}
+	*/
+	/*TODO follow tutorial
+	public int getYOffset() {
+		//TODO follow tutorial
+	}
+	*/
+	public void render() {
+		//TODO follow tutorial
+	}
+	public Gravity getGravity() {
+		return this.gravity;
+	}
+	public void setGravity(Gravity newGravity) {
+		this.gravity = newGravity;
+	}
+	public void backgroundRender() {
+		//TODO follow tutorial
+	}
+}
