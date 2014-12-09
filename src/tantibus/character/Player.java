b9a395dacc9f7cd590e92afe4995463da58232f6
Tologon Eshimkanov <eshimkanovt@wit.edu>
2014-12-08 22:22:00 -0500

    Added minor styling & required changes & new method.

--- src/tantibus/character/Player.java
+++ src/tantibus/character/Player.java
@@ -3,15 +3,21 @@ package tantibus.character;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
-public class Player extends Character {
-	
+import tantibus.physics.AACollisionDetection;
 
+public class Player extends Character {
+	AACollisionDetection collisionDetection;
 	public Player(float x, float y) throws SlickException{
 		super (x,y);
-		setSprite(new Image("images/characters/p1_walk01.png"));
-		setMovingAnimation(new Image[]{new Image("images/characters/p1_walk01.png"), new Image ("images/characters/p1_walk02.png"), new Image("images/characters/p1_walk03.png"),new Image("p1_walk04.png"),new Image("images/characters/p1_walk05.png"),new Image("images/characters/p1_walk06.png"),new Image("images/characters/p1_walk07.png"),new Image("images/characters/p1_walk08.png"),new Image("images/characters/p1_walk09.png"),new Image("images/characters/p1_walk10.png"),new Image("images/characters/p1_walk11.png")}, 125);
+		setSprite(new Image("images/characters/p1_spritesheet.png"));
+		setMovingAnimation(new Image[]{new Image("images/characters/p1_walk01.png"),
+									new Image ("images/characters/p1_walk02.png"), new Image("images/characters/p1_walk03.png"),
+									new Image("images/characters/p1_walk04.png"),new Image("images/characters/p1_walk05.png"),
+									new Image("images/characters/p1_walk06.png"),new Image("images/characters/p1_walk07.png"),
+									new Image("images/characters/p1_walk08.png"),new Image("images/characters/p1_walk09.png"),
+									new Image("images/characters/p1_walk10.png"),new Image("images/characters/p1_walk11.png")}, 125);
 		
-		//TODO   boundingShape = new AABoundingRect(x+3, y, 26, 32);
+		collisionDetection = new AACollisionDetection(x + 3, y, 26, 32);
 		
 		accelerationSpeed = 0.002f;
 		decelerationSpeed = 0.002f;
@@ -19,8 +25,7 @@ public class Player extends Character {
 		maxFallSpeed = 0.4f;
 	}
 
-	/*TODO public void updateBoundingShape(){
-				boundingShape.updatePosition(x+4,y);
-			}
-	*/
+	public void updateCollisionDetection() {
+		collisionDetection.updatePosition(x + 4, y);
+	}
 }
