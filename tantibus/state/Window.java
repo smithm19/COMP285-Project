07c6b1d58f5af080f008181c6c003c94c676360b
Tologon Eshimkanov <eshimkanovt@wit.edu>
2014-12-11 01:56:39 -0500

    MAJOR OVERHAUL (MO): updates, styling, and comments.

--- tantibus/state/Window.java
+++ tantibus/state/Window.java
@@ -1,39 +1,41 @@
 package tantibus.state;
 
+// slick2D library import
 import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.Game;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-
-public class Window extends StateBasedGame{
+public class Window extends StateBasedGame {
 	
-	public static final int windowWidth = 800;
-	public static final int windowHeight = windowWidth / 16*9; //adjusts to width
-	public static final float scale = (float) (1.25*((double)windowWidth/800));
-	public static final String GameName = "Tantibus";
+	// sets width and height of the game window in accordance with aspect ratio
+	public static final int 	WINDOW_WIDTH 	= 1280;
+	public static final int 	WINDOW_HEIGHT 	= WINDOW_WIDTH / 16 * 9; // automatically adjusts to width
+	public static final boolean FULLSCREEN 		= false;
 	
+	// base screen resolution: 1280 x 720
+	// tiles: 32 x 32 size, but they are adjusted to 40 x 40 size at 1280 x 720 (aspect ratio of 1.25)
+	private static final double	ASPECT_RATIO 	= 1.25;
+	public static final float 	SCALE 			= (float) (ASPECT_RATIO * ((double) WINDOW_WIDTH / 800));
+	public static final String 	GAME_NAME 		= "Tantibus";
 	
 	public Window() {
-		
-		super(GameName);
+		super(GAME_NAME);
 	}
 
 	
-	public void initStatesList(GameContainer arg0) throws SlickException {
-		
-		addState(new GameState("level_0")); //name of file that loads
-		
+	public void initStatesList(GameContainer gameContainer) throws SlickException {
+		// creates a game state, which does the whole logic and rendering for individual levels
+		addState(new GameState("level_1"));
 		this.enterState(0);
 	}
 	
-	public static void main(String[] args) throws SlickException{
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Window());
 		
-		AppGameContainer app = new AppGameContainer ((Game) new Window());
-		
-		app.setDisplayMode(windowWidth, windowHeight, false);//false = fullscreen
-		app.setTargetFrameRate(60); //60 fps
+		// sets the width, the height, and the fullscreen mode
+		app.setDisplayMode(WINDOW_WIDTH, WINDOW_HEIGHT, FULLSCREEN);
+		app.setTargetFrameRate(60);
 		
 		app.start(); 
 	}
